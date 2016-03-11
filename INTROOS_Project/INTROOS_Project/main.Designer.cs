8fbb24a6158c0bc37627eb5d71718d1101f6fe46
Paolo Nablo <juan_nablo@dlsu.edu.ph>
2016-03-11 22:06:55 +0800

    uploaded program to github

---
+++ INTROOS_Project/INTROOS_Project/main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace INTROOS_Project
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.processTxt = new System.Windows.Forms.TextBox();
+            this.processListView = new System.Windows.Forms.ListView();
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(12, 12);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(153, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(13, 123);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(152, 23);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // processTxt
+            // 
+            this.processTxt.Location = new System.Drawing.Point(13, 42);
+            this.processTxt.Name = "processTxt";
+            this.processTxt.Size = new System.Drawing.Size(152, 20);
+            this.processTxt.TabIndex = 2;
+            // 
+            // processListView
+            // 
+            this.processListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.processListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName});
+            this.processListView.Location = new System.Drawing.Point(192, 12);
+            this.processListView.Name = "processListView";
+            this.processListView.Size = new System.Drawing.Size(431, 353);
+            this.processListView.TabIndex = 4;
+            this.processListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 250;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 377);
+            this.Controls.Add(this.processListView);
+            this.Controls.Add(this.processTxt);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "main";
+            this.Text = "Process Manager";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.TextBox processTxt;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ListView processListView;
+    }
+}
+
---
+++ INTROOS_Project/INTROOS_Project/main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INTROOS_Project
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            string processString = processTxt.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = processString;
+            process.Start();
+            loadProcessList();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            loadProcessList();
+        }
+
+        private void loadProcessList(){
+
+            processListView.Items.Clear();
+            Process[] processList = Process.GetProcesses();
+            foreach (Process process in processList){
+
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.ProcessName);
+                item.Tag = process;
+                processListView.Items.Add(item);
+                processListView.View = View.Details;
+
+                }
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = processListView.SelectedItems[0];
+            Process process = (Process)item.Tag;
+            process.Kill();
+            loadProcessList();
+        }
+    }
+}
